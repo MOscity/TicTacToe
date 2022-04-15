38c3c94eaaccdda5937aef734425fe0dca1b8f34
MOscity <matthias.oscity@me.com>
2022-04-15 17:34:09 +0200

    Complete Game

---
+++ tic-tac-toe/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+<div class="container">
+<span ref='confetti'></span>
+  <Board @winner='fireConfetti'/>
+</div>
+</template>
+
+<script>
+import Board from './components/Board.vue'
+import { confetti } from '../../node_modules/dom-confetti/src/main.js';
+
+export default {
+  name: 'App',
+  components: {
+    Board,
+  },
+  methods: {
+    fireConfetti () {
+      confetti(this.$refs.confetti)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.container {
+    display: grid;
+    place-items: center;
+    gap: 4rem;
+    position: relative;
+}
+</style>
